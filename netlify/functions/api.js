56b28340e99f8f721ef5121c8c5a8c7e8b406d7e
Alessandra Scarpellini <scaalessandra@icloud.com>
2023-11-17 12:48:30 +0100

    reset apikey from .env

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,12 +1,12 @@
 import fetch from 'node-fetch'
-const apiKey = process.env.API_KEY
+const API_KEY = process.env.API_KEY
 
 export const handler = async (event, context) => {
-    console.log(apiKey)
+    console.log(API_KEY)
     const cityName = "amsterdam";
    //const API_KEY = process.env.API_KEY
 
-    const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=a1bed3af7d71b323ab4af9036fcd943d&units=metric`
+    const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`
 
     const response = await fetch(API_URL);
     const data = await response.json()
