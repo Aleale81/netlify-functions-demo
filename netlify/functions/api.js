278554315640c14dd8606095227950b0aad3b476
Alessandra Scarpellini <scaalessandra@icloud.com>
2023-11-17 12:35:28 +0100

    hard coded apikey

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -6,7 +6,7 @@ export const handler = async (event, context) => {
     const cityName = "amsterdam";
    //const API_KEY = process.env.API_KEY
 
-    const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`
+    const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=a1bed3af7d71b323ab4af9036fcd943d&units=metric`
 
     const response = await fetch(API_URL);
     const data = await response.json()
