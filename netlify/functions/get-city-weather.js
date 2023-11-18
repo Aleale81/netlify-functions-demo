5348017ee094d45c28f87de6e10a8fc3f16ee331
Alessandra Scarpellini <scaalessandra@icloud.com>
2023-11-18 12:48:25 +0100

    add console.log for error

--- netlify/functions/get-city-weather.js
+++ netlify/functions/get-city-weather.js
@@ -3,25 +3,28 @@ import axios from "axios";
 export const handler = async (event, context) => {
 
     const cityName = event.body;
+    console.log('City', cityName)
     const API_KEY = process.env.API_KEY
 
     const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`
 
     try {
         const response = await axios.get(API_URL);
+
         return {
             statusCode: 200,
             body: JSON.stringify({
-                data: response.json()
+                data: response
             })
         }
     } catch (error) {
-        return {
-            statusCode: 400,
-            body: JSON.stringify({
-                data: error.message.json()
-            })
-        }
+        console.log(error)
+        // return {
+        //     statusCode: 400,
+        //     body: JSON.stringify({
+        //         data: error.message.json()
+        //     })
+        // }
     }
     // const data = await response.json()
     // console.log(data)
