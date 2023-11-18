07533747c89859775395c244d16dcd51080a8a4a
Alessandra Scarpellini <scaalessandra@icloud.com>
2023-11-18 13:26:42 +0100

    use json parrse in body

--- netlify/functions/get-city-weather.js
+++ netlify/functions/get-city-weather.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 export const handler = async (event, context) => {
-    console.log("EVENT", event.body)
-    const {city}= event.body
+    console.log("EVENT", JSON.parse(event.body))
+    const city = JSON.parse(event.body)
     console.log('City', city)
     
     const API_KEY = process.env.API_KEY
 
-    const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+    const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${city.city}&appid=${API_KEY}&units=metric`
 
     try {
         const response = await axios.get(API_URL);
