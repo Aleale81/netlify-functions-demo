d02251867c3f58679f0eab7979c9bea7a91118a8
Alessandra Scarpellini <scaalessandra@icloud.com>
2023-11-18 12:41:21 +0100

    create netlify function that receive data to send req to api

---
+++ netlify/functions/get-city-weather.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+export const handler = async (event, context) => {
+
+    const cityName = event.body;
+    const API_KEY = process.env.API_KEY
+
+    const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`
+
+    try {
+        const response = await axios.get(API_URL);
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                data: response.json()
+            })
+        }
+    } catch (error) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({
+                data: error.message.json()
+            })
+        }
+    }
+    // const data = await response.json()
+    // console.log(data)
+
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,61 @@
-
-
-import axios from 'axios'
-import './App.css'
+import axios from "axios";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [city, setCity] = useState("berlin");
+  const [result, setResult] = useState(null)
+  const [error, setError] = useState(undefined)
   // ${import.meta.env.VITE_BACKEND_URL}
-  const triggerRequest = () => {
-    axios.get(`${import.meta.env.VITE_BACKEND_URL}/api`)
-      .then(response => {
-        console.log(response)
-      })
-      .catch( e => {
-        console.log(e)
-      })
-  }
+
+  // const triggerRequest = () => {
+  //   axios
+  //     .get(`${import.meta.env.VITE_BACKEND_URL}/api`)
+  //     .then((response) => {
+  //       console.log(response);
+  //     })
+  //     .catch((e) => {
+  //       console.log(e);
+  //     });
+  // };
+
+  const handleForm = (e) => {
+    e.preventDefault();
+    console.log("CITY", city);
+    axios
+    .post(`${import.meta.env.VITE_BACKEND_URL}/get-city-weather`, city)
+    .then((response) => {
+      console.log(response);
+      setResult(response.data)
+    })
+    .catch((err) => {
+      console.log(err.message);
+      setError(err.message)
+    });
+  };
 
   return (
     <>
-      
       <h1>Netlify Functions</h1>
 
-      <button onClick={triggerRequest}>Trigger API request</button>    
-      
+      {/* <button onClick={triggerRequest}>Trigger API request</button> */}
+
+      <form onSubmit={handleForm}>
+        <input
+          type="text"
+          name="city"
+          value={city}
+          onChange={(e) => {
+            setCity(e.tartget.value.toLowerCase());
+          }}
+          autoFocus
+        />
+        <button type="submit">Submit</button>
+      </form>
+      {result && <h2>{result.city}</h2>}
+      {error && <p>{error}</p>}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
